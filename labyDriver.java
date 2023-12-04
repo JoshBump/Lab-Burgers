98f56d941a458c209c6f726337ebd00f646cf216
JoshBump <143889817+JoshBump@users.noreply.github.com>
2023-12-04 12:59:32 -0500

    Add files via upload
    Game done

--- labyDriver.java
+++ labyDriver.java
@@ -12,18 +12,18 @@ public static Cards deck = new Cards();
 
 public static Player red = new Player(1,deck);
 
+public static Player blue = new Player(2,deck);
+
+public static  Player yellow = new Player(3,deck);
+
+public static   Player green = new Player(4,deck);
+
     public static void main(String[] args) {
 
-        GraphicsLab graphics = new GraphicsLab();
-        for(int i = 0; i < 50; i++){
-            graphics.TileGraphics(i+1, graphics.AssignTile(board.getTreasure(i), board.getType(i), board.getRotation(i)));
-
-        }
-        GraphicsLab.frame.repaint();
-
-
-        Player blue = new Player(2,deck);
-        Player yellow = new Player(3,deck);
-        Player green = new Player(4,deck);
-
-}}
+            GraphicsLab graphics = new GraphicsLab();
+            for (int i = 0; i < 50; i++) {
+                graphics.TileGraphics(i + 1, graphics.AssignTile(board.getTreasure(i), board.getType(i), board.getRotation(i)));
+            }
+            GraphicsLab.frame.repaint();
+}
+}
